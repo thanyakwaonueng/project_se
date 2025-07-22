8efd5bea9783abf236d78751c2c84b5a213905c7
thanyakwaonueng <kwaonueng@gmail.com>
2025-07-22 20:22:07 +0700

    setting up temporary relation named User and setting up dummy path on backend/index.js for testing GET and POST request to User table and the change that reflected on db, the point is everything work as expected.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 // Required External Modules
 const express = require('express');
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient();
 
 // App Variables
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 4000; // Use environment variable PORT or default to 4000
+
+//*****
+//THIS IS DUMMY ENDPOINT ADDING FOR TESTING THE DB CONNECTION PURPOSE
+// Create user
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+// Get all users
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+//*****
+
 
 // App Configuration (optional, can include middleware, view engine setup, etc.)
 // app.set('view engine', 'pug');
@@ -19,3 +44,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
