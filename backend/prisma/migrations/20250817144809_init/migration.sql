17575308fb6761ff0d89d33f8f94898fbfb83404
thanyakwaonueng <kwaonueng@gmail.com>
2025-09-11 10:32:02 +0700

    remove stupid module & file

--- backend/prisma/migrations/20250817144809_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "VenueProfile" ADD COLUMN     "latitude" DOUBLE PRECISION,
-ADD COLUMN     "longitude" DOUBLE PRECISION;
-
--- CreateIndex
-CREATE INDEX "VenueProfile_latitude_longitude_idx" ON "VenueProfile"("latitude", "longitude");
