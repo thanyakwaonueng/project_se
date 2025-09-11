3525a6dc98616ef1b65335d34877ca28654d1ce9
bimsaysbecool <150535396+bimsaysbecool@users.noreply.github.com>
2025-09-11 16:48:02 +0700

    update footer bar

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -1,52 +1,61 @@
-import '../css/Footer.css'
+import '../css/Footer.css';
 
 export default function Footer() {
   return (
-    <div className="bottom-bar">
-      <div className="follow-us-box">
-        <p className="follow-us">FOLLOW US:</p>
-        <hr className="social-divider" />
+    <div className="footer">
+      <div className="footer-container">
 
-        <div className="social-media">
-          <div className="formline-social">
-            <a className="btn" id="btn-social"
-              href="https://www.facebook.com/chiangmaioriginal/?locale=th_TH"
-              role="button">
-              <img src="/img/facebook.png" alt="Facebook" className="btn-full-image" />
-            </a>
-            <p className="social-name">FACEBOOK</p>
+        {/* Logo + Social */}
+        <div className="footer-section">
+          <div className="footer-logo">
+            <img src="/img/logonavbar.png" alt="Chiangmai Original" className="logo"/>
+            {/* <p className="brand-text">It is about sound & crowd in Chiang Mai Town</p> */}
           </div>
-
-          <div className="formline-social">
-            <a className="btn" id="btn-social"
-              href="https://www.instagram.com/cnx.og/?hl=en"
-              role="button">
-              <img src="/img/instagram.png" alt="Instagram" className="btn-full-image" />
+          <div className="social-icons">
+            <a href="https://www.facebook.com/chiangmaioriginal/?locale=th_TH" target="_blank">
+              <img src="/img/facebook.png" alt="Facebook"/>
             </a>
-            <p className="social-name">INSTAGRAM</p>
-          </div>
-
-          <div className="formline-social">
-            <a className="btn" id="btn-social"
-              href="https://www.youtube.com/c/ChiangmaiOriginal"
-              role="button">
-              <img src="/img/youtube.png" alt="youtube" className="btn-full-image" />
+            <a href="https://www.instagram.com/cnx.og/?hl=en" target="_blank">
+              <img src="/img/instagram.png" alt="Instagram"/>
             </a>
-            <p className="social-name">YOUTUBE</p>
-          </div>
-
-          <div className="formline-social">
-            <a className="btn" id="btn-social"
-              href="https://www.tiktok.com/@chiangmaioriginal"
-              role="button">
-              <img src="/img/tiktok.png" alt="tiktok" className="btn-full-image" />
+            <a href="https://www.youtube.com/c/ChiangmaiOriginal" target="_blank">
+              <img src="/img/youtube.png" alt="YouTube"/>
             </a>
-            <p className="social-name">TIKTOK</p>
+            <a href="https://www.tiktok.com/@chiangmaioriginal" target="_blank">
+              <img src="/img/tiktok.png" alt="TikTok"/>
+            </a>
           </div>
+          {/* <p className="brand-text">It is about sound & crowd in Chiang Mai Town</p> */}
         </div>
+
+        {/* Contact Us */}
+        <div className="footer-section">
+          <h4>CONTACT US</h4>
+          <p>chiangmai.original@gmail.com</p>
+          <p>083 680 6868</p>
+          <p>Chiang Mai, Thailand</p>
+        </div>
+
+        <div className="footer-section">
+          <h4>JOIN US</h4>
+          <p>Be part of the Chiang Mai Original community</p>
+          <a href="/signup" className="signup-btn">Get Started ↗</a>
+        </div>
+
+
+        {/* Subscribe */}
+        {/* <div className="footer-section">
+          <h4>SUBSCRIBE</h4>
+          <p>Enter your email to get notified about our events</p>
+          <div className="subscribe-box">
+            <input type="email" placeholder="Email" />
+            <button>✉</button>
+          </div>
+        </div> */}
       </div>
 
-      <p className="bottom-text">2025 CHIANG MAI ORIGINAL</p>
+      <hr className="footer-divider" />
+      <p className="footer-bottom">2025 Chiang Mai Original</p>
     </div>
   );
 }
