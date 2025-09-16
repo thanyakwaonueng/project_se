058fd319762aa7bea0c8252f89196b2628f3ce28
bimsaysbecool <150535396+bimsaysbecool@users.noreply.github.com>
2025-09-17 00:50:05 +0700

    update sign up page

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { AuthProvider } from '../lib/auth';
 
 export default function Layout() {
+  const location = useLocation();  // ดึง path ปัจจุบัน
+  const hideNavandFooter = location.pathname === '/signup'; // หน้า signup จะไม่แสดง
+
   return (
     <AuthProvider>
       <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-        <Navbar />
+        {!hideNavandFooter && <Navbar />}
+
+        {!hideNavandFooter && (
           <div
             style={{
               width: "90%",
@@ -17,12 +22,13 @@ export default function Layout() {
               margin: "30px auto", 
             }}
           ></div>
+        )}
 
-          <main style={{ flex: 1 }}>
-          
+        <main style={{ flex: 1 }}>
           <Outlet />
         </main>
-        <Footer />
+
+        {!hideNavandFooter && <Footer />}
       </div>
     </AuthProvider>
   );
